c9fab677eeb9f33a4c51d3003454634c7b117f82
jplayer1127 <jordan@Jordans-MacBook-Pro.local>
2015-07-21 18:54:10 -0400

    Profile changes

--- client/people/profileEdit.js
+++ client/people/profileEdit.js
@@ -8,15 +8,23 @@ Template.profileEdit.helpers({
 Template.profileEdit.events({
 	"submit #profile-edit-form": function(event){
 		event.preventDefault();
-		var bio = event.target.bio.value;
+		var street= event.target.street.value;
+		var city= event.target.city.value;
+		var state= event.target.state.value;
+		var zipcode= event.target.zipcode.value;
 		var firstName = event.target.firstName.value;
 		var lastName = event.target.lastName.value;
-		var userName = event.target.userName.value;
+		var email = event.target.email.value;
 		Meteor.users.update(this._id,
-			{$set:{'profile.bio':bio, 
+			{$set:{ 
+					'profile.street':street,
+					'profile.city':city,
+					'profile.state':state,
+					'profile.zipcode':zipcode,
 					'profile.firstName':firstName, 
 					'profile.lastName':lastName,
-					'profile.userName':userName}});
+					'profile.email':email}});
+
 		Router.go('/profile/'+this._id);
 		
 	}
